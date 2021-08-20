81dd5dccf20243646bfc4c005e627042bc105fef
kagehayashi <kagehayashi@github.com>
2021-08-20 15:08:29 -0700

    Cleaned

---
+++ astar.py
@@ -0,0 +1,127 @@
+import sys
+ 
+import pygame
+from pygame.locals import *
+ 
+pygame.init()
+
+width, height = 500, 500
+rows, cols = 5, 5
+cell_w = width / cols
+cell_h = height / rows
+
+screen = pygame.display.set_mode((width, height))
+fps = 1
+fpsClock = pygame.time.Clock()
+
+
+class Cell():
+    def __init__(self, i, j):
+        self.i = i
+        self.j = j
+        self.f = 0
+        self.g = 0
+        self.h = 0
+        self.neighbors = []
+
+    def show(self, color):
+        rect = pygame.Rect(self.i*cell_w, self.j*cell_h, 
+                           cell_w, cell_h)
+        pygame.draw.rect(screen, color, rect, 0) # The filled rect
+        pygame.draw.rect(screen, (0,0,0), rect, 1) # The border of the rect
+
+    def find_neighbors(self, grid):
+        '''
+        Returns a list of all the neighboring cells
+        '''
+        #north, south, east, west neighbor locations
+        nsew = ((self.i-1, self.j),(self.i+1, self.j),
+                (self.i, self.j+1),(self.i, self.j-1))
+        
+        # north-east, south-east, north-west, south-west neighbor locations
+        neseswnw = ((self.i-1, self.j+1),(self.i+1, self.j+1),
+                    (self.i-1, self.j-1),(self.i+1, self.j-1))
+        
+        all_directions = nsew + neseswnw
+        
+        for i_prime, j_prime in all_directions:
+            # Bounds check
+            if i_prime < 0 or i_prime > rows-1 \
+                or j_prime < 0 or j_prime > cols-1:
+                continue
+            else:
+                self.neighbors.append(grid[i_prime][j_prime])
+        
+def calculate_manhanttan_heuristics(current_cell, end_cell):
+	return abs(current_cell.i - end_cell.i) + abs(current_cell.j - end_cell.j)
+
+grid = [[Cell(i, j) for j in range(cols)] for i in range(rows)]
+for row in grid:
+    for cell in row:
+        cell.find_neighbors(grid)
+
+start = grid[0][0]
+end = grid[4][4]
+
+open_cells = [start]
+closed_cells = []
+
+
+# Game loop
+while True:
+    screen.fill((0, 0, 0))
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Update
+    if (open_cells != []):
+        lowest_f_index = 0
+        for i in range(len(open_cells)):
+            if open_cells[i].f < open_cells[lowest_f_index].f:
+                lowest_f_index = i
+        
+        current = open_cells[lowest_f_index]
+        # print("Current cell: ", current.i, current.j)
+
+        if open_cells[lowest_f_index] == end:
+            print("FOUND!")
+            break
+
+        open_cells.remove(current)
+        closed_cells.append(current)
+        # print("Open cells: ", open_cells)
+        # print("Closed cells: ", closed_cells)
+
+        for neighbor in current.neighbors:
+            # print("Neighbor: ", neighbor.i, neighbor.j)
+            if neighbor not in closed_cells:
+                tempG = current.g + 1
+                if neighbor in open_cells:
+                    if tempG < neighbor.g:
+                        neighbor.g = tempG
+                else:
+                    neighbor.g = tempG
+                    open_cells.append(neighbor)
+                
+                neighbor.h = calculate_manhanttan_heuristics(neighbor, end)
+                neighbor.f = neighbor.g + neighbor.h
+                # print(neighbor.f)
+    else:
+        pass
+
+    # Draw
+    for row in grid:
+        for cell in row:
+            cell.show((255,255,255))
+
+    for cell in open_cells:
+        cell.show((0,255,0))
+
+    for cell in closed_cells:
+        cell.show((255,0,0))
+
+    pygame.display.flip()
+    fpsClock.tick(fps)
